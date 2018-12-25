efd05645e124a69158bee38c203e382e13617578
Abdul Rafay Modi <rafaymodi18@gmail.com>
2018-12-25 11:29:32 +0500

    Add project files.

---
+++ Vidly_test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_test.Startup))]
+namespace Vidly_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
